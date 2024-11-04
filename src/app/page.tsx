75eddf1ea75c15d851bf0e40c862f73f1aad48f7
Hisham Abi Farah <v-habifarah@microsoft.com>
2024-11-04 12:12:28 +0200

    sync deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,15 +29,15 @@ export default function Page() {
             world.
           </p>
 
-          <Link href="/signup" className="block text-center hover:underline">
+          <Link href="/signup" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
               Sign up
             </Link>
             
-          <Link href="/login" className="block text-center hover:underline">
+          <Link href="/login" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
               Sign In
             </Link>
             
-          <Link href="/home" className="block text-center hover:underline">
+          <Link href="/home" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
               Home
             </Link>
         </div>
