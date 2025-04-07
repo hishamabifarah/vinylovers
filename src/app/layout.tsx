7a25b5f55d5d5799e26b23b459da862264dea115
Hisham Abi Farah (WPP Group USA LLC) <v-habifarah@microsoft.com>
2025-04-07 20:12:12 +0300

    removed duplicate meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -84,35 +84,12 @@ export default function RootLayout({
     <html lang="en">
       <head>
          <meta name="google-site-verification" content="-7GGj5Z4Y23rNlOrBmYAqRoA16epUrp2UahAIgeFtuY" />
-        {/* Viewport Meta Tag */}
+         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charSet="utf-8" />
-        <meta name="robots" content="index, follow"/>
-        {/* Canonical URL */}
         <link rel="canonical" href={siteUrl} />
-
-        {/* Favicon */}
         <link rel="icon" href="/favicon.ico" />
-
-        {/* Theme Color */}
         <meta name="theme-color" content="#000000" />
-
-        {/* Apple Touch Icon */}
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-
-        {/* Explicit OpenGraph tags for WhatsApp */}
-        <meta property="og:image" content={ogImageUrl} />
-        {/* <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:image:alt" content="Vinylovers - Connect with Vinyl Lovers & Share Your Collection" /> */}
-        <meta property="og:title" content="Vinylovers - Connect with Vinyl Lovers & Share Your Collection" />
-        <meta
-          property="og:description"
-          content="Join the vinyl community to share your collection, discover new music, and connect with fellow vinyl enthusiasts."
-        />
-        <meta property="og:url" content={siteUrl} />
-        <meta property="og:type" content="website" />
-        <meta property="og:site_name" content="Vinylovers" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ReactQueryProvider>
